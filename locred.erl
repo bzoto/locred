0323667766c422486b0ad10433eb708530abb2d2
Matteo Pradella <matteo.pradella@gmail.com>
2016-12-14 16:56:01 +0100

    look-back/look-ahead automaton

--- locred.erl
+++ locred.erl
@@ -14,6 +14,7 @@
          ,reduction_star/2
          ,add_borders/2
          ,check_system/1
+         ,automaton_states/1
         ]).
 
 -export([format_factors/1]).
@@ -27,8 +28,8 @@ check_string(String) ->
     {Res, _} = lists:foldl(fun check_string_help/2, {true, odd}, String),
     Res.
 
-check_string_help(Char,  {true, even}) -> {not(lists:member(Char, "<.>")), odd};
-check_string_help(Char,  {true, odd})  -> {lists:member(Char, "<.>"), even};
+check_string_help(Char,  {true, even}) -> {not(lists:member(Char, "[.]")), odd};
+check_string_help(Char,  {true, odd})  -> {lists:member(Char, "[.]"), even};
 check_string_help(_Char, {false, _V})  -> {false, _V}.
 
 
@@ -36,7 +37,7 @@ borders(1) -> "#";
 borders(Size) -> lists:append("#.",borders(Size-2)).
 
 add_borders(String, Size) -> lists:append([borders(Size),
-                                           "<", String, ">",
+                                           "[", String, "]",
                                            borders(Size)]).
 format_factors(Set) ->
     io:format("{~n"),
@@ -70,7 +71,7 @@ check_factors(String, System) ->
 
 % Check if a System is conflictual
 conflictual(Str1, Str2) ->
-    F = fun(X) -> not(lists:member(X, "<.>")) end,
+    F = fun(X) -> not(lists:member(X, "[.]")) end,
     Str1 =/= Str2 andalso lists:filter(F,Str1) =:= lists:filter(F, Str2).
 
 check_system(System) ->
@@ -78,7 +79,7 @@ check_system(System) ->
     Confl = sets:fold(fun(X,B) ->
                               sets:fold(fun(Y,B1) ->
                                                 case conflictual(X,Y) of
-                                                    true -> [{conflict, X, Y} | B1];
+                                                    true  -> [{conflict, X, Y} | B1];
                                                     false -> B1
                                                 end
                                         end, B, Factors)
@@ -95,16 +96,40 @@ check_system(System) ->
 %    io:format("Conflicts: ~p~n", [Confl]),
 %    Confl.
 
+% Look-back/Look-ahead automaton construction
+automaton_states(System) ->
+    {system, Factors, _Size} = System,
+    States = sets:fold(fun(X,B) ->
+                               sets:fold(fun(Y,B1) ->
+                                                 % this is to avoid spurious beginnings
+                                                 Flag = hd(Y) =/= hd("#") andalso lists:last(X) =/= hd("#"), 
+                                                 V = lists:append(X,tl(Y)),
+                                                 case check_factors(V, System) andalso Flag of
+                                                     true ->  R1 = [tl(V)];
+                                                     false -> R1 = []
+                                                 end,
+                                                 V1 = lists:append(lists:droplast(X),Y),
+                                                 case check_factors(V1, System) andalso Flag of
+                                                     true ->  R2 = [lists:droplast(V1) | R1];
+                                                     false -> R2 = R1
+                                                 end,
+                                                 sets:union(sets:from_list(R2),B1)
+                                         end, B, Factors)
+                       end, sets:new(), Factors),
+    io:format("Automaton:"),
+    format_factors(States),
+    States.
+
 
 
 % Reduction and Reduction*
 reduction(String, System) ->
-    {ok, Handle} = re:compile("<[^<>]+>"),
+    {ok, Handle} = re:compile("\\[[^\\[\\]]+\\]"),
     case re:split(String, Handle, [{parts, 2},{return, list}]) of
         [L,R] ->
-            HP = [lists:append([L,"<",R]),
+            HP = [lists:append([L,"[",R]),
                   lists:append([L,".",R]),
-                  lists:append([L,">",R])],
+                  lists:append([L,"]",R])],
             case lists:map(fun(X) -> check_factors(X, System) end, HP) of
                 [true, false, false] -> [V, _, _] = HP, {ok, V};
                 [false, true, false] -> [_, V, _] = HP, {ok, V};
@@ -131,16 +156,25 @@ reduction_star(String, System) ->
 % used as a script:
 % escript locred.erl
 main(_V) ->
-    S0 = bordered_factors_of("e>-.-.e<*.e<*.-.e",5),
-    S1 = sets:union(S0, bordered_factors_of("e>-.e<*.-.e",5)),
-    S2 = sets:union(S1, bordered_factors_of("e>-.e>-.e",5)),
+    S0 = bordered_factors_of("e]-.-.e[*.e[*.-.e",5),
+    S1 = sets:union(S0, bordered_factors_of("e]-.e[*.-.e",5)),
+    S2 = sets:union(S1, bordered_factors_of("e]-.e]-.e",5)),
     S3 = sets:union(S2, bordered_factors_of("-.-.e",5)),
     S4 = sets:union(S3, bordered_factors_of("-.e",5)),
     S5 = sets:union(S4, bordered_factors_of("*.e",5)),
     S6 = sets:union(S5, bordered_factors_of("*.-.e",5)),
-    check_system({system, S6, 5}),
+    Sys = {system, S6, 5},
+    check_system(Sys),
     format_factors(S6),
-    Sys = {system, S6,5},
-    reduction_star(add_borders("e>-.e>-.-.e>-.e",5), Sys),
+    reduction_star(add_borders("e]-.e]-.-.e]-.e",5), Sys),
     io:format("~n"),
-    reduction_star(add_borders("e>*.e>*.-.e>*.e",5), Sys).
+    reduction_star(add_borders("e]*.e]*.-.e]*.e",5), Sys),
+    
+    %%% Automaton
+    
+    Anbn = bordered_factors_of("a[a.b]b",3),
+    format_factors(Anbn),
+    San = {system, Anbn, 3},
+    check_system(San),
+    automaton_states(San).
+
